3e720a8fad9c87b87e9fbdb6b329f9702047eb12
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2022-06-12 20:05:02 -0300

    'ej3'

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,19 @@
 function MaxValue (shares) {
   // Your code here:
+  let ganancias = 0;
+  let total = 0
+  for(let i = 0; i < shares.length - 1; i++){
+    for(let j = i+1; j < shares.length; j++){
+      total = shares[j] - shares[i]
+      if(total > ganancias){
+        ganancias = total
+      }
+    }
+  }
 
+  return ganancias
 }
+
+
 
 module.exports = MaxValue
