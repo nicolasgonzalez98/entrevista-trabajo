b0f90f65dc65689d98941a6093dff3b0b5120ea7
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2022-06-13 01:43:32 -0300

    'ej5'

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,16 @@
 function mdArraySum (arr) {
   // Your code here:
+  let suma = 0
 
+  for(let i = 0; i < arr.length; i++){
+    if(Array.isArray(arr[i])){
+      suma += mdArraySum(arr[i])
+    }else{
+      suma += arr[i]
+    }
+  }
+
+  return suma
 }
 
 module.exports = mdArraySum
--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,11 +1,38 @@
 class Node {
   // Your code here:
-
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
 }
 
 class MinStack {
   // Your code here:
+  constructor(value){
+    this.top = null
+  }
 
+  push(value){
+    if(!this.top){
+      this.top = new Node(value)
+    }else{
+      let nodo = new Node(value)
+      nodo.next = this.top
+      this.top = nodo
+    }
+  }
+
+  pop(){
+    
+  }
+
+  min(){
+
+  }
+
+  peek(){
+
+  }
 }
 
 module.exports = {
